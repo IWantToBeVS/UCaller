88f8cafdfe03d6beeaddd0b09e85486a6decb89a
IWantToBeVS <120201521+IWantToBeVS@users.noreply.github.com>
2024-07-15 14:48:41 +0530

    Started creating UCaller
    Designed basic frontend components.

---
+++ my-app/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import MobileNav from "./MobileNav";
+
+const Navbar = () => {
+    return (
+        <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+            <Link href="/" className="flex items-center gap-1">
+                <Image src="/icons/Video.svg" width={32} height={32} alt="Yoom Logo" className="max-sm:size-10">
+                </Image>
+                <p className="text-[26px] font-extrabold text-white max-sm:hidden">Yoom</p>
+            </Link>
+            <div className="flex-between gap-5">
+                {/* Clerk - User Management */}
+                <MobileNav></MobileNav>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
